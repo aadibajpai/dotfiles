68941f2c10b7df387f5ba8c647aa94f1ebb46371
Aadi Bajpai <hello@aadibajpai.com>
2020-10-10 09:41:20 +0530

    add comment to function

--- fish/functions/confess.fish
+++ fish/functions/confess.fish
@@ -1,3 +1,3 @@
-function confess
+function confess --description just VandyHacks stuff lol
 	curl --silent --location --request POST 'https://SECRET-URL' --header 'Content-Type: application/x-www-form-urlencoded' --data-urlencode "text=$argv" --data-urlencode 'response_type=in_channel' | jq .text
 end
