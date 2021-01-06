117a35dcaeae7f70591028ef9bfc8b60b03fc031
Aadi Bajpai <hello@aadibajpai.com>
2021-01-06 23:52:06 +0530

    remove prev stuff

--- fish/functions/shorten.fish
+++
@@ -1,7 +0,0 @@
-function shorten --description 'VandyHacks URL shortener service' --argument NAME URL
-    curl --location -X POST "https://vhl.ink" \
-        -H "x-preshared-key: $SHORTEN_PSK" \
-        -H "Content-Type: application/x-www-form-urlencoded" \
-        --data-urlencode "url=$URL" \
-        --data-urlencode "path=$NAME"
-end
